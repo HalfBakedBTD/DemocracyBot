be6a19a0d158be2f7517d4b4ccc3604b90cb4b45
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-17 18:33:12 -0400

    Update top.js

--- commands/top.js
+++ commands/top.js
@@ -3,9 +3,9 @@ const respect = require("../respect.json");
 
 exports.run = async (bot, message, args) => {
   if(!respect[message.author.id]) {
-	    respect[message.author.id] = {
+	  respect[message.author.id] = {
 		  role: 'citizen',
-		  respect: 0
+		  respect: 2
 	  };
   }
   let topID = message.author.id;
@@ -20,21 +20,29 @@ exports.run = async (bot, message, args) => {
   let topIDTen = 0;
   
   let top = respect[message.author.id].respect;
-  let top = 0;
-  let top = 0;
-  let top = 0;
-  let top = 0;
-  let top = 0;
-  let top = 0;
-  let top = 0;
-  let top = 0;
-  let top = 0;
+  let topTwo = 0;
+  let topThree = 0;
+  let topFour = 0;
+  let topFive = 0;
+  let topSix = 0;
+  let topSeven = 0;
+  let topEight = 0;
+  let topNine = 0;
+  let topTen = 0;
   bot.users.filter(u => u.id !== `${message.author.id}`).forEach(user => {
-  if(!respect[user.id]) {
-    respect[user.id] = {
-      role: 'citizen',
-		  respect: 0
-    };
+    if(!respect[user.id]) {
+      respect[user.id] = {
+        role: 'citizen',
+	      respect: 2
+      };
+    }
+		let uRep = respect[user.id].respect;
+		if (uRep > top) {
+			topIDTwo = topID
+			topTwo = top
+			topID = user.id
+			top = uRep
+		}
   }
 
 }
