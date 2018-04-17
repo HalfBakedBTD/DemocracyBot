84dc06a08a94f4355e3ed4136e7ef1226695abee
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-17 18:14:40 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,10 +4,10 @@ exports.run = async (bot, message, args) => {
 	message.reply(`I am privately messaging you all my commands.`)
     let botEmbed = new Discord.RichEmbed()
     .setColor('#fff200')
-    .addField("Government Commands:", "`-respect`: shows your current respect status.\n`-pay`: lets you pay your respects to a user.\n`-announce`: lets you announce something.\n`-poll`: lets you make a poll.");
-    message.author.send(botEmbed);
+    .addField("Government Commands:", "`-me`: shows your current respect status.\n`-pay`: lets you pay your respects to a user.\n`-top`: shows top respected users.\n`-update`:  updates roles for all users.");
+ message.author.send(botEmbed)     
 }
-
+	      
 module.exports.help = {
   name: "help"
 }
