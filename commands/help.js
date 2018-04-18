873553796d56912263d97c396e8d105551d66d0f
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-17 23:13:19 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,10 +6,10 @@ exports.run = async (bot, message, args) => {
   .setColor('#fff200')
   .addField("Government Commands:", "`-me`: shows your current respect status.\n`-pay`: lets you pay your respects to a user.\n`-top`: shows top respected users.\n`-update`:  updates roles for all users.");
 	message.author.send(botEmbed) 
-  let botEmbed = new Discord.RichEmbed()
+  let linkEmbed = new Discord.RichEmbed()
   .setColor('#e67e22')
   .addField("Links:", "[Invite](https://discordapp.com/oauth2/authorize?client_id=435913715016269826&permissions=8&scope=bot)");
-	message.author.send(botEmbed)
+	message.author.send(linkEmbed)
 }
 	      
 module.exports.help = {
