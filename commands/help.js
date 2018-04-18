e33c8eb87531edb31edcf128d693165b61293404
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-18 14:20:03 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,11 +4,11 @@ exports.run = async (bot, message, args) => {
 	message.reply(`I am privately messaging you all my commands.`)
   let botEmbed = new Discord.RichEmbed()
   .setColor('#fff200')
-  .addField("Government Commands:", "`-me`: shows your current respect status.\n`-pay`: lets you pay your respects to a user.\n`-top`: shows top respected users.\n`-update`:  updates roles for all users.");
+  .addField("Government Commands:", "`-me`: shows your current respect status.\n`-respect`: lets you respect a user giving them and yourself more respect.\n`-disgrace` - disgracess a user taking away respect.\n`-top`: shows top respected users.\n`-update`:  updates roles for all users.");
 	message.author.send(botEmbed) 
   let linkEmbed = new Discord.RichEmbed()
   .setColor('#e67e22')
-  .addField("Links:", "[Invite](https://discordapp.com/oauth2/authorize?client_id=435913715016269826&permissions=8&scope=bot)");
+  .addField("Links:", "[Invite](https://discordapp.com/oauth2/authorize?client_id=435913715016269826&permissions=8&scope=bot) | [Donate](https://www.patreon.com/halfbakedpatreon)");
 	message.author.send(linkEmbed)
 }
 	      
